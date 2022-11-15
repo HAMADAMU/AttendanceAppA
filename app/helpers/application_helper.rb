9d9faa6167f05f65ce032cba94b55e0e5e8e5544
RT <ryusuke1133@gmail.com>
2022-11-16 07:53:49 +0900

    users controller作るところまで

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_title(page_name = "")
+    base_title = "AttendanceAppA"
+    if page_name.blank?
+      base_title
+    else
+      page_name + " | " + base_title
+    end
+  end
 end
